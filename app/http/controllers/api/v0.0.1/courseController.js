4401816bb3f0230a2200cef1512d877e47c975c1
alirezajs <alireza.varmaghani@gmail.com>
2019-02-02 00:04:09 +0330

    filter data for restful

--- app/http/controllers/api/v0.0.1/courseController.js
+++ app/http/controllers/api/v0.0.1/courseController.js
@@ -2,6 +2,7 @@ const controller = require('app/http/controllers/api/controller');
 const Course = require('app/models/course');
 const Episode = require('app/models/episode');
 const Comment = require('app/models/comment');
+const passport = require('passport');
 
 
 class courseController extends controller {
@@ -9,11 +10,12 @@ class courseController extends controller {
     async courses(req, res, next) {
         try {
             let page = req.query.page || 1;
-            let courses = await Course.paginate({}, { page, sort: { createdAt: 1 }, limit: 12 });
-           
+            let courses = await Course.paginate({}, { page, sort: { createdAt: 1 }, limit: 12, populate: [{ path: 'categories' }, { path: 'user' }] });
+
 
             res.json({
-                data: courses,
+                // data: courses,
+                data: this.filterCoursesData(courses),
                 status: 'success'
             })
 
@@ -22,51 +24,125 @@ class courseController extends controller {
         }
     }
 
-    async singleCourse(req , res) {
+    async singleCourse(req, res) {
         try {
-            let course = await Course.findByIdAndUpdate(req.params.course , { $inc : { viewCount : 1}})
-                                            .populate([
-                                                {
-                                                    path : 'user',
-                                                    select : 'name'
-                                                } ,
-                                                {
-                                                    path : 'episodes',
-                                                    options : { sort : { number : 1} }
-                                                }
-                                            ]);
-            if(! course ) return this.failed('چنین دوره ای یافت نشد', res , 404);
+            let course = await Course.findByIdAndUpdate(req.params.course, { $inc: { viewCount: 1 } })
+                .populate([
+                    {
+                        path: 'user',
+                        select: 'name'
+                    },
+                    {
+                        path: 'episodes',
+                        options: { sort: { number: 1 } }
+                    },
+                    {
+                        path: 'categories',
+                        select: 'name slug'
+                    }
+                ]);
+
+            if (!course) return this.failed('چنین دوره ای یافت نشد', res, 404);
 
 
-            res.json({
-                data : course,
-                status : 'success'
-            })
+            passport.authenticate('jwt', { session: false }, (err, user, info) => {
+
+                res.json({
+                    data: this.filterCourseData(course, user),
+                    status: 'success'
+                });
+
+            })(req, res);
 
         } catch (err) {
-           this.failed(err.message , res);
+            this.failed(err.message, res);
         }
     }
 
-    async commentForSingleCourse(req , res ) {
+    async commentForSingleCourse(req, res) {
         try {
-            let comments = await Comment.find({ course : req.params.course , parent : null , approved : true })
+            let comments = await Comment.find({ course: req.params.course, parent: null, approved: true })
                 .populate([
                     {
-                        path : 'user',
-                        select : 'name'
+                        path: 'user',
+                        select: 'name'
                     },
                     {
-                        path : 'comments',
-                        match : {
-                            approved : true
+                        path: 'comments',
+                        match: {
+                            approved: true
                         },
-                        populate : { path : 'user' , select : 'name'}
-                    }   
+                        populate: { path: 'user', select: 'name' }
+                    }
                 ])
             return res.json(comments);
         } catch (err) {
-            this.failed(err.message , res);
+            this.failed(err.message, res);
+        }
+    }
+
+
+    filterCoursesData(courses) {
+        return {
+            ...courses,
+            docs: courses.docs.map(course => {
+                return {
+                    id: course.id,
+                    title: course.title,
+                    slug: course.slug,
+                    body: course.body,
+                    image: course.thumb,
+                    categories: course.categories.map(cate => {
+                        return {
+                            name: cate.name,
+                            slug: cate.slug
+                        }
+                    }),
+                    user: {
+                        id: course.user.id,
+                        name: course.user.name
+                    },
+                    price: course.price,
+                    createdAt: course.createdAt
+                }
+            })
+        }
+    }
+
+    filterCourseData(course, user) {
+        return {
+            id: course.id,
+            title: course.title,
+            slug: course.slug,
+            body: course.body,
+            image: course.thumb,
+            categories: course.categories.map(cate => {
+                return {
+                    name: cate.name,
+                    slug: cate.slug
+                }
+            }),
+            user: {
+                id: course.user.id,
+                name: course.user.name
+            },
+            episodes: course.episodes.map(episode => {
+                return {
+                    time: episode.time,
+                    downloadCount: episode.downloadCount,
+                    viewCount: episode.viewCount,
+                    commentCount: episode.commentCount,
+                    id: episode.id,
+                    title: episode.title,
+                    body: episode.body,
+                    type: episode.type,
+                    number: episode.number,
+                    createdAt: episode.createdAt,
+                    download: episode.download(!!user, user)
+                }
+            }),
+            price: course.price,
+            createdAt: course.createdAt
         }
     }
 }
--- app/http/controllers/api/v0.0.1/homeController.js
+++ app/http/controllers/api/v0.0.1/homeController.js
@@ -6,24 +6,77 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
 class homeController extends controller {
-    
-    async user(req , res) {
-        res.json(req.user);
+
+    async user(req, res) {
+        try {
+            let user = await req.user.populate({ path: "roles", populate: [{ path: "permissions" }] }).execPopulate();
+            return res.json({
+                data: this.filterUserData(user),
+                status: "success"
+
+            })
+        }
+
+        catch (error) {
+            this.failed(error, res)
+        }
     }
 
-    async history(req , res , next) {
+    async history(req, res, next) {
         try {
             let page = req.query.page || 1;
-            let payments = await Payment.paginate({ user : req.user.id } , { page , sort : { createdAt : -1} , limit : 20 , populate : 'course'});
-        
+            let payments = await Payment.paginate({ user : req.user.id } , { page , sort : { createdAt : -1} , limit : 20 , populate : [ { path : 'course'} , { path : 'user' , select : 'name email'}]});
+
             res.json({
-                data : payments,
-                status : 'success'
-            })     
+                data: this.filterPaymentData(payments),
+                status: 'success'
+            })
         } catch (err) {
-            this.failed(err.message , res);
+            this.failed(err.message, res);
+        }
+    }
+
+    filterPaymentData(payments) {
+        return {
+            ...payments,
+            docs : payments.docs.map(pay => {
+                return {
+                    payment : pay.payment,
+                    resnumber : pay.resnumber,
+                    price : pay.price,
+                    user: {
+                        name : pay.user.name,
+                        email : pay.user.email
+                    }
+                }
+            })
+        }
+    }
+
+     filterUserData(user) {
+        return {
+            id : user.id,
+            admin : user.admin,
+            name : user.name,
+            email : user.email,
+            createdAt : user.createdAt,
+            vipTime : user.vipTime,
+            vipType : user.vipType,
+            roles : user.roles.map(role => {
+                return {
+                    name : role.name ,
+                    label : role.label,
+                    permissions : role.permissions.map(per => {
+                        return {
+                            name : per.name ,
+                            label : per.label
+                        }
+                    })
+                }
+            })
         }
     }
 }
+
 
 module.exports = new homeController();
